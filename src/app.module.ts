e0aab8ae06ae220b0f5522ea912ecdee289c9e5c
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-15 02:21:34 +0400

    feature/updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app/app.gateway';
+
+import { UsersModule } from './users/users.module';
 import { InfraModule } from './infra/infra/infra.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    PrismaModule,
     InfraModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
