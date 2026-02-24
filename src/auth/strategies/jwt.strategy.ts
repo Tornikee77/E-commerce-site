95be457352e1e3f3a4f4733bb16a9c4631ec50f2
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-25 01:23:31 +0400

    update

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,21 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common'; // დაამატე UnauthorizedException
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from '../token-payload.interface';
+import { UsersService } from '../../users/users.service'; // შემოიტანე UsersService
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private readonly usersService: UsersService, // დააინექტირე სერვისი
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request.cookies.Authentication,
+        (request: Request) => request?.cookies?.Authentication,
       ]),
       secretOrKey: configService.getOrThrow('JWT_SECRET'),
     });
   }
-  validate(payload: TokenPayload) {
-    return payload;
+
+  // NestJS ავტომატურად გამოიძახებს ამას, თუ ტოკენი ვალიდურია
+  async validate(payload: TokenPayload) {
+    // ბაზიდან ამოგვაქვს მომხმარებელი ID-ით
+    const user = await this.usersService.getUser({ id: payload.userId });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    // რასაც აქ დააბრუნებ, ის იქნება შენი @CurrentUser
+    return user;
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateuserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { jwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import { TokenPayload } from 'src/auth/token-payload.interface';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +20,11 @@ export class UsersController {
   @UseInterceptors(NoFilesInterceptor)
   createUser(@Body() request: CreateuserDto) {
     return this.usersService.createUser(request);
+  }
+
+  @Get('me')
+  @UseGuards(jwtAuthGuard)
+  getMe(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
