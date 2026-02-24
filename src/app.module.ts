7554d0e7b098a74d37d25ed94674bd280a9c09e9
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-02-24 20:30:57 +0400

    feature/current user decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,6 @@ import { AuthModule } from './auth/auth.module';
     }),
 
     UsersModule,
-
     AuthModule,
   ],
   controllers: [AppController],
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
-  // constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login() {}
+  login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.login(user, response);
+  }
 }
