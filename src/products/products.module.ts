3b8d2ed07296e3280c9ee3a1c79487f77e3940f9
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-01 23:36:08 +0400

    feature/products gateway

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProductGateWay } from './product.gateway';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, ProductGateWay],
   exports: [ProductsService],
 })
 export class ProductsModule {}
